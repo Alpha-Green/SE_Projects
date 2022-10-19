340153cf4e8f7e424ad094242bfa768672ba152b
Alpha-Green <erabornoah@gmail.com>
2022-10-19 12:44:59 +0100

    An even shorter and easier way to all add 3 numbers

--- Addup.c
+++ Addup.c
@@ -3,15 +3,9 @@
    int a, b, c, sum;
 
    printf("Welcome to this simple addition calculator.\n");
-   printf("Please type the first number and press ENTER: ");
-   scanf("%d", &a);
-      printf("Please type the second number and press ENTER: ");
-      scanf("%d", &b);
-    printf("Please type the third number and press ENTER: ");
-    scanf("%d", &c);
-
+   printf("Please enter all 3 numbers and press ENTER: ");
+    scanf("%d%d%d", &a, &b,&c);
     sum = a + b + c;
-
     printf ("The answer is %d", sum);
     printf("\nThank you!");
  }
